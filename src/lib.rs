14df6e1547caad769f72237664d1961596d3086a
John Halbert <john@fivemile.software>
2020-02-06 19:10:29 -0800

    Fix assertion error
    - avro_rs changed display value of ValidationError to user Validation
    error instead of Decoding error, which broke tests.  Fixes those tests.

--- src/lib.rs
+++ src/lib.rs
@@ -488,7 +488,7 @@ fn to_bytes_no_transfer_wrong() {
         result,
         Err(SRCError::new(
             "Could not get avro bytes",
-            Some("Decoding error: value does not match schema"),
+            Some("Validation error: value does not match schema"),
             false,
         ))
     )
@@ -1091,7 +1091,7 @@ mod tests {
             result,
             Err(SRCError::new(
                 "Could not get avro bytes",
-                Some("Decoding error: value does not match schema"),
+                Some("Validation error: value does not match schema"),
                 false,
             ))
         )
